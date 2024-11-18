b59ca0b0b04c5a1dbe030a79a529554449831d66
natanaeldeholanda <natan.holanda@gmail.com>
2024-11-18 20:54:10 -0300

    Assisti até a aula 73 Do While

---
+++ src/controle/WhileIndeterminado.java
@@ -0,0 +1,16 @@
+package controle;
+
+import java.util.Scanner;
+
+public class WhileIndeterminado {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        String valor = "";
+        while (!valor.equalsIgnoreCase("sair")) {
+            System.out.print("Você diz: ");
+            valor = entrada.nextLine();
+        }
+        System.out.println("Saiu");
+        entrada.close();
+    }
+}
